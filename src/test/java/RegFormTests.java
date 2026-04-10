742b0e3a65a8a61ed9b140c9daea8b01e096be6c
Inna Shishova <sh201608@yandex.ru>
2026-04-10 22:20:12 +0400

    changed state and city

--- src/test/java/RegFormTests.java
+++ src/test/java/RegFormTests.java
@@ -2,11 +2,9 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selectors.byText;
 
 public class RegFormTests {
@@ -17,17 +15,15 @@ public class RegFormTests {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
-
     }
 
     @Test
     void fillFormTest() {
-
         String firstName = "Ivan";
         String lastName = "Petrov";
-        String Email = "test1@test2.com";
+        String email = "test1@test2.com";
         String gender = "Female";
-        String Number = "1234567890";
+        String number = "1234567890";
         String birthDay = "15";
         String birthMonth = "October";
         String birthYear = "2025";
@@ -39,65 +35,46 @@ public class RegFormTests {
         String state = "Haryana";
         String city = "Panipat";
 
-
-        //Открытие страницы 'Practice Form'//
         open("/automation-practice-form");
 
         executeJavaScript("""
-    document.getElementById('fixedban')?.remove();
-    document.querySelector('footer')?.remove();
-""");
+            document.getElementById('fixedban')?.remove();
+            document.querySelector('footer')?.remove();
+        """);
 
-        //Name//
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
-
-        //Email//
-        $("#userEmail").setValue(Email);
-
-        //Gender//
+        $("#userEmail").setValue(email);
         $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(number);
 
-        //Mobile//
-        $("#userNumber").setValue(Number);
-
-        //Date of Birth//
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOption(birthYear);
         $(".react-datepicker__month-select").selectOption(birthMonth);
         $(".react-datepicker__month").$(byText(birthDay)).click();
 
-        //Subjects//
         $("#subjectsInput").setValue(subject).pressEnter();
-
-        //Hobbies//
         $("#hobbiesWrapper").$(byText(hobby)).click();
-
-        //Picture//
         $("#uploadPicture").uploadFromClasspath(picture);
 
-        //Current Address//
         $("#currentAddress").scrollTo().shouldBe(visible);
         $("#currentAddress").setValue(address);
 
-
-        //State and City//
         $("#state").click();
-        $("#stateCity-wrapper").$(byText(state)).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText(city)).click();
+        $(byText(state)).click();
 
-        //Submit
+        $("#city").click();
+        $(byText(city)).click();
+
         $("#submit").click();
 
-        //Проверка таблицы
+        //Таблица проверки
         $("[class=modal-header]").shouldHave(text("Thanks for submitting the form"));
-
         $(".table").shouldHave(text(firstName));
         $(".table").shouldHave(text(lastName));
-        $(".table").shouldHave(text(Email));
+        $(".table").shouldHave(text(email));
         $(".table").shouldHave(text(gender));
-        $(".table").shouldHave(text(Number));
+        $(".table").shouldHave(text(number));
         $(".table").shouldHave(text(expectedBirthDate));
         $(".table").shouldHave(text(subject));
         $(".table").shouldHave(text(hobby));
@@ -105,12 +82,7 @@ public class RegFormTests {
         $(".table").shouldHave(text(address));
         $(".table").shouldHave(text(state + " " + city));
 
-        //Закрытие окна
         $("#closeLargeModal").click();
-
-
     }
 }
-
-
 
